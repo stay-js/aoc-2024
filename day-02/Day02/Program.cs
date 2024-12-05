0d541b8378797398670cd39c794b597fa0bf7fea
Nagy Zétény <nagyzeteny6@gmail.com>
2024-12-05 15:38:27 +0100

    add day 2 part 2 solution

--- day-02/Day02/Program.cs
+++ day-02/Day02/Program.cs
@@ -6,16 +6,32 @@ const int MAX_DIFF = 3;
 
 Console.WriteLine("demo-input:");
 Console.WriteLine(CountSafeMeasurements(demoInput));
-
+Console.WriteLine(CountSafeMeasurementsWithRemoving(demoInput));
 
 Console.WriteLine("\ninput:");
 Console.WriteLine(CountSafeMeasurements(input));
+Console.WriteLine(CountSafeMeasurementsWithRemoving(input));
 
 static int CountSafeMeasurements(IEnumerable<string> lines) => lines.Sum(line =>
 {
     int[] numbers = line.Split().Select(int.Parse).ToArray();
     if (IsSafe(numbers, true) || IsSafe(numbers, false)) return 1;
     return 0;
+});
+
+static int CountSafeMeasurementsWithRemoving(IEnumerable<string> lines) => lines.Sum(line =>
+{
+    int[] numbers = line.Split().Select(int.Parse).ToArray();
+    if (IsSafe(numbers, true) || IsSafe(numbers, false)) return 1;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        int[] newNumbers = [.. numbers[..i], .. numbers[(i + 1)..]];
+        if (IsSafe(newNumbers, true) || IsSafe(newNumbers, false)) return 1;
+    }
+
+    return 0;
+
 });
 
 static bool IsSafe(int[] numbers, bool increasing)
