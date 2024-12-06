e2e195bea0ffe4732f69dd09fc54f6457ffed836
Nagy Zétény <nagyzeteny6@gmail.com>
2024-12-06 20:14:50 +0100

    add day 4 part 2

--- day-04/main.py
+++ day-04/main.py
@@ -31,6 +31,26 @@ def first_part(string: str) -> int:
     return count
 
 
+def second_part(string: str) -> int:
+    TARGET_STRINGS = ["MAS", "SAM"]
+
+    count = 0
+
+    data = string.split('\n')
+
+    height = len(data)
+    width = len(data[0])
+
+    for i in range(1, height-1):
+        for j in range(1, width-1):
+            if data[i][j] == 'A' \
+                    and data[i-1][j-1] + 'A' + data[i+1][j+1] in TARGET_STRINGS \
+                    and data[i-1][j+1] + 'A' + data[i+1][j-1] in TARGET_STRINGS:
+                count += 1
+
+    return count
+
+
 def main():
     with open("demo-input.txt") as file:
         demo_input = file.read()
@@ -40,9 +60,11 @@ def main():
 
     print("demo-input:")
     print(first_part(demo_input))
+    print(second_part(demo_input))
 
     print("\ninput:")
     print(first_part(real_input))
+    print(second_part(real_input))
 
 
 if __name__ == '__main__':
