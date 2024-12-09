552e57dda800a95dbb2edf6e215a6137a1e8634b
Nagy Zétény <nagyzeteny6@gmail.com>
2024-12-10 00:11:54 +0100

    simplify day 9 part 1

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,62 +1,48 @@
 use std::fs::read_to_string;
 
 #[derive(Clone, Copy)]
-enum FileOrFreeSpace {
-    File(u32),
+enum Block {
+    File(u16),
     FreeSpace,
 }
 
-fn get_leftmost_free_space(disk: &Vec<FileOrFreeSpace>) -> Option<usize> {
-    for (idx, item) in disk.iter().enumerate() {
-        if let FileOrFreeSpace::FreeSpace = item {
-            return Some(idx);
-        }
-    }
-
-    return None;
+fn get_leftmost_free_space(disk: &Vec<Block>) -> Option<usize> {
+    return disk
+        .iter()
+        .position(|item| matches!(item, Block::FreeSpace));
 }
 
-fn get_rightmost_file(disk: &Vec<FileOrFreeSpace>) -> Option<usize> {
-    for (idx, item) in disk.iter().enumerate().rev() {
-        if let FileOrFreeSpace::File(_) = item {
-            return Some(idx);
-        }
-    }
-
-    return None;
+fn get_rightmost_file(disk: &Vec<Block>) -> Option<usize> {
+    return disk.iter().rposition(|item| matches!(item, Block::File(_)));
 }
 
 fn first_part(input: &str) -> u64 {
     let mut index = 0;
     let mut disk = Vec::new();
 
-    for (i, c) in input.trim().chars().enumerate() {
+    for (i, c) in input.chars().enumerate() {
+        let count = c.to_digit(10).unwrap() as usize;
+
         if i % 2 == 0 {
-            for _ in 0..c.to_digit(10).unwrap() {
-                disk.push(FileOrFreeSpace::File(index));
-            }
+            disk.extend(vec![Block::File(index); count]);
 
             index += 1;
             continue;
         }
 
-        for _ in 0..c.to_digit(10).unwrap() {
-            disk.push(FileOrFreeSpace::FreeSpace);
-        }
+        disk.extend(vec![Block::FreeSpace; count]);
     }
 
-    while let Some(a) = get_leftmost_free_space(&disk) {
-        if let Some(b) = get_rightmost_file(&disk) {
-            if b < a {
-                break;
-            }
-
-            (disk[a], disk[b]) = (disk[b], disk[a]);
+    while let (Some(a), Some(b)) = (get_leftmost_free_space(&disk), get_rightmost_file(&disk)) {
+        if a > b {
+            break;
         }
+
+        (disk[a], disk[b]) = (disk[b], disk[a]);
     }
 
     return disk.iter().enumerate().fold(0, |acc, (idx, item)| {
-        if let FileOrFreeSpace::File(file) = item {
+        if let Block::File(file) = item {
             return acc + *file as u64 * idx as u64;
         }
 
