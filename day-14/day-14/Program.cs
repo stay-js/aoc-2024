09451f7487482ff5b5f6b8b0022287317e3614a5
Nagy Zétény <nagyzeteny6@gmail.com>
2024-12-16 15:09:10 +0100

    add day 14 part 1

---
+++ day-14/day-14/Program.cs
@@ -0,0 +1,47 @@
+﻿using day_14;
+
+const int TIME = 100;
+
+const int DEMO_WIDTH = 11;
+const int DEMO_HEIGHT = 7;
+
+const int INPUT_WIDTH = 101;
+const int INPUT_HEIGHT = 103;
+
+string demoInput = await File.ReadAllTextAsync("demo-input.txt");
+string input = await File.ReadAllTextAsync("input.txt");
+
+Console.WriteLine("demo-input:");
+Console.WriteLine(FirstPart(demoInput, DEMO_WIDTH, DEMO_HEIGHT, TIME));
+
+Console.WriteLine("\ninput:");
+Console.WriteLine(FirstPart(input, INPUT_WIDTH, INPUT_HEIGHT, TIME));
+
+static int FirstPart(string input, int width, int height, int time)
+{
+    var robots = input.Split('\n').Select(x => new Robot(x)).ToList();
+
+    for (int _ = 0; _ < time; _++)
+    {
+        robots.ForEach(x => x.UpdatePosition(width, height));
+    }
+
+    int middleX = width / 2;
+    int middleY = height / 2;
+
+    int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+
+    foreach (var robot in robots)
+    {
+        var (x, y) = robot.Position;
+
+        if (x == middleX || y == middleY) continue;
+
+        if (x < middleX && y < middleY) q1++;
+        else if (x >= middleX && y < middleY) q2++;
+        else if (x < middleX && y >= middleY) q3++;
+        else if (x >= middleX && y >= middleY) q4++;
+    }
+
+    return q1 * q2 * q3 * q4;
+}
---
+++ day-14/day-14/Robot.cs
@@ -0,0 +1,33 @@
+﻿using Position = (int x, int y);
+
+namespace day_14
+{
+    public class Robot
+    {
+        public Position Position { get; set; }
+        public Position Velocity { get; set; }
+
+        public Robot(string line)
+        {
+            string[] parts = line.Split(' ');
+
+            Position = ParsePosition(parts[0]);
+            Velocity = ParsePosition(parts[1]);
+        }
+
+        public void UpdatePosition(int width, int height)
+        {
+            Position = (
+                (Position.x + Velocity.x + width) % width,
+                (Position.y + Velocity.y + height) % height
+            );
+        }
+
+        private static Position ParsePosition(string input)
+        {
+            int[] position = input.Split('=')[1].Split(',').Select(int.Parse).ToArray();
+
+            return (position[0], position[1]);
+        }
+    }
+}
